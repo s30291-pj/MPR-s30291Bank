a15e191db1bccef171fc15da15f00d567611e795
s30291-pj <s30291@pjwstk.edu.pl>
2025-01-25 10:42:36 +0100

    initial project

---
+++ src/main/java/pl/edu/pjwstk/s30291/projects/mpr/bank/S30291BankApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.s30291.projects.mpr.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30291BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S30291BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pjwstk/s30291/projects/mpr/bank/S30291BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.s30291.projects.mpr.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30291BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
