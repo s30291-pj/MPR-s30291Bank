3cd0d1a3809e678005d4e5190f85878a5c21e9f5
s30291-pj <s30291@pjwstk.edu.pl>
2025-01-25 12:42:05 +0100

    last commit

--- src/test/java/pl/edu/pjwstk/s30291/projects/mpr/bank/account/service/AccountServiceTest.java
+++ src/test/java/pl/edu/pjwstk/s30291/projects/mpr/bank/account/service/AccountServiceTest.java
@@ -1,8 +1,11 @@
 package pl.edu.pjwstk.s30291.projects.mpr.bank.account.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import pl.edu.pjwstk.s30291.projects.mpr.bank.account.Account;
 import pl.edu.pjwstk.s30291.projects.mpr.bank.account.repository.AccountRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,7 +29,14 @@ public class AccountServiceTest {
 	public void testExists() {
 		when(accounts.existsById(any())).thenReturn(true);
 	
-		service.exists(UUID.randomUUID());
+		assertTrue(service.exists(UUID.randomUUID()));
+	}
+	
+	@Test
+	public void testNotExists() {
+		when(accounts.existsById(any())).thenReturn(false);
+	
+		assertFalse(service.exists(UUID.randomUUID()));
 	}
 	
 }
